87b3abe77f04e817333f99ea9538bb7634eb1366
Nathan Mohr <nlm9380@gmail.com>
2016-12-06 11:59:23 -0600

    Finished PostDao

--- src/main/java/org/launchcode/blogz/models/dao/PostDao.java
+++ src/main/java/org/launchcode/blogz/models/dao/PostDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.launchcode.blogz.models.Post;
+import org.launchcode.blogz.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
